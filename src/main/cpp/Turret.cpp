1abe1c3eee27a5a70c7dfb2312a247092c5470b1
team 1771's personal <41267535+team-1771@users.noreply.github.com>
2022-02-14 17:28:26 -0500

    Shoot while moving?

--- src/main/cpp/Turret.cpp
+++ src/main/cpp/Turret.cpp
@@ -3,9 +3,11 @@
 #include "LimeLight.hpp"
 #include "ngr.hpp"
 #include "PID_CANSparkMax.hpp"
+#include "Trajectory.hpp"
 
 #include <wpi/numbers>
 #include <units/angle.h>
+#include <complex>
 
 #include <cmath>
 
@@ -40,6 +42,7 @@ extern LimeLight camera; // camera from Robot.cpp
 static PID_CANSparkMax turretTurnyTurny{PORT, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
 static Turret::POSITION position = Turret::POSITION::ZERO;
 static bool tracking = false;
+static rev::SparkMaxRelativeEncoder encoder;
 
 /******************************************************************/
 /*                   Public Function Definitions                  */
@@ -57,6 +60,7 @@ void Turret::init()
     turretTurnyTurny.SetOutputRange(-TRAVERSE_SPEED, TRAVERSE_SPEED);
     turretTurnyTurny.SetPositionRange(Turret::POSITION::MAX_LEFT, Turret::POSITION::MAX_RIGHT);
     turretTurnyTurny.SetTarget(Turret::POSITION::ZERO);
+    encoder = turretTurnyTurny.GetEncoder();
 }
 
 bool Turret::goToPosition(Turret::POSITION const &pos, double const &tolerance)
@@ -85,7 +89,36 @@ Turret::visionState Turret::visionTrack(Turret::POSITION const &initPosition, do
     if (camera.hasTarget())
     {
         // auto const target = result.GetBestTarget();
-        auto const x_offset_deg = units::degree_t{camera.getX()} + CAMERA_X_OFFSET;
+        auto const [robot_x_speed, robot_y_speed, robot_omega_speed] = Trajectory::getEstimatedSpeeds();
+
+        auto x_offset_deg = units::degree_t{camera.getX()} + CAMERA_X_OFFSET;
+
+        if (robot_x_speed > 1_mps || robot_y_speed > 1_mps)
+        {
+
+            // Add position conversion ratio to get to degrees
+            frc::Rotation2d const turret_angle = units::degree_t{encoder.GetPosition()};
+
+            auto const [turret_relative_front_back_robot_speed, turret_relative_left_right_robot_speed, _] =
+                frc::ChassisSpeeds::FromFieldRelativeSpeeds(robot_x_speed, robot_y_speed, robot_omega_speed, turret_angle);
+
+            // From here on, x = horizontal (left/right), y = verticle (front/back)
+
+            std::complex<double> const robot_speed{turret_relative_left_right_robot_speed.value(), turret_relative_front_back_robot_speed.value()};
+
+            auto const rotations_per_second = 6700 / 60;
+            units::radians_per_second_t const radians_per_second{rotations_per_second / (2 * wpi::numbers::pi)};
+            units::meters_per_second_t const meters_per_second{radians_per_second.value() * units::meter_t{3_in}.value()};
+           
+            auto shoot_speed = std::polar(meters_per_second.value() / 2, units::radian_t{x_offset_deg}.value());
+
+            auto const ball_speed = shoot_speed - robot_speed;
+
+            // unit circle angle converted to being front-centered, CW
+            units::degree_t const movement_angle = units::radian_t{std::arg(ball_speed)};
+
+            x_offset_deg = units::radian_t{std::arg(ball_speed)};
+        }
         double const x_offset_ticks = x_offset_deg.value() * TICKS_PER_DEGREE;
 
         double const x_pos = turretTurnyTurny.encoder.GetPosition();
