17eef504335c63d686a94776454b5bdabc600315
MrRedness <redness123@protonmail.com>
2022-02-16 23:52:59 -0500

    Aim logic fixes?

--- src/main/cpp/Turret.cpp
+++ src/main/cpp/Turret.cpp
@@ -42,7 +42,6 @@ extern LimeLight camera; // camera from Robot.cpp
 static PID_CANSparkMax turretTurnyTurny{PORT, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
 static Turret::POSITION position = Turret::POSITION::ZERO;
 static bool tracking = false;
-static rev::SparkMaxRelativeEncoder encoder;
 
 /******************************************************************/
 /*                   Public Function Definitions                  */
@@ -60,7 +59,6 @@ void Turret::init()
     turretTurnyTurny.SetOutputRange(-TRAVERSE_SPEED, TRAVERSE_SPEED);
     turretTurnyTurny.SetPositionRange(Turret::POSITION::MAX_LEFT, Turret::POSITION::MAX_RIGHT);
     turretTurnyTurny.SetTarget(Turret::POSITION::ZERO);
-    encoder = turretTurnyTurny.GetEncoder();
 }
 
 bool Turret::goToPosition(Turret::POSITION const &pos, double const &tolerance)
@@ -91,47 +89,47 @@ Turret::visionState Turret::visionTrack(Turret::POSITION const &initPosition, do
         // auto const target = result.GetBestTarget();
         auto const [robot_x_speed, robot_y_speed, robot_omega_speed] = Trajectory::getEstimatedSpeeds();
 
-        auto x_offset_deg = units::degree_t{camera.getX()} + CAMERA_X_OFFSET;
+        auto offset_deg = units::degree_t{camera.getX()} + CAMERA_X_OFFSET;
+
+        double const turret_pos_ticks = turretTurnyTurny.encoder.GetPosition();
 
         if (robot_x_speed > 1_mps || robot_y_speed > 1_mps)
         {
 
-            // Add position conversion ratio to get to degrees
-            frc::Rotation2d const turret_angle = units::degree_t{encoder.GetPosition()};
+            units::degree_t const turret_pos_deg{turret_pos_ticks / TICKS_PER_DEGREE};
 
-            auto const [turret_relative_front_back_robot_speed, turret_relative_left_right_robot_speed, _] =
-                frc::ChassisSpeeds::FromFieldRelativeSpeeds(robot_x_speed, robot_y_speed, robot_omega_speed, turret_angle);
+            units::degree_t const target_deg = offset_deg + turret_pos_deg;
 
-            // From here on, x = horizontal (left/right), y = verticle (front/back)
+            auto const target_relative_speeds =
+                frc::ChassisSpeeds::FromFieldRelativeSpeeds(robot_x_speed, robot_y_speed, robot_omega_speed,
+                                                            target_deg);
 
-            std::complex<double> const robot_speed{turret_relative_left_right_robot_speed.value(), turret_relative_front_back_robot_speed.value()};
+            auto const target_relative_y_speed = target_relative_speeds.vy.value();
+            units::radian_t const offset_rad = offset_deg;
+            auto const target_relative_angle = offset_rad.value() - atan2(target_relative_y_speed, 0);
+            
+            std::complex<double> const target_relative_robot_speed =
+                std::polar(target_relative_y_speed, 
+               target_relative_angle);
 
-            auto const rotations_per_second = 6700 / 60;
-            units::radians_per_second_t const radians_per_second{rotations_per_second / (2 * wpi::numbers::pi)};
-            units::meters_per_second_t const meters_per_second{radians_per_second.value() * units::meter_t{3_in}.value()};
-           
-            auto shoot_speed = std::polar(meters_per_second.value() / 2, units::radian_t{x_offset_deg}.value());
+            // Speed of shooter wheel is 53.46 m/s
+            auto shoot_speed = std::polar(53.46 / 2, offset_rad.value());
 
-            auto const ball_speed = shoot_speed - robot_speed;
+            auto const ball_speed = shoot_speed - target_relative_robot_speed;
 
-            // unit circle angle converted to being front-centered, CW
-            units::degree_t const movement_angle = units::radian_t{std::arg(ball_speed)};
-
-            x_offset_deg = units::radian_t{std::arg(ball_speed)};
+            offset_deg = units::radian_t{std::arg(ball_speed)};
         }
-        double const x_offset_ticks = x_offset_deg.value() * TICKS_PER_DEGREE;
 
-        double const x_pos = turretTurnyTurny.encoder.GetPosition();
-        double const x_target = x_pos + x_offset_ticks;
+        double const target_ticks = turret_pos_ticks + (offset_deg.value() * TICKS_PER_DEGREE);
 
         static auto prev_offset_deg = 0_deg;
-        if (prev_offset_deg == x_offset_deg) // prevents reusing outdated data
-            return {true, std::abs(x_offset_deg.value()) < tolerance};
-        prev_offset_deg = x_offset_deg;
+        if (prev_offset_deg == offset_deg) // prevents reusing outdated data
+            return {true, std::abs(offset_deg.value()) < tolerance};
+        prev_offset_deg = offset_deg;
 
-        turretTurnyTurny.SetTarget(x_target);
+        turretTurnyTurny.SetTarget(target_ticks);
 
-        return {true, std::abs(x_offset_deg.value()) < tolerance};
+        return {true, std::abs(offset_deg.value()) < tolerance};
     }
 
     return {false, false};
